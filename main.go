f6e7d96d9f91b06810ecd325e77de40b80694fc8
Ilham Aris Hartadi <44136041+arieshta@users.noreply.github.com>
2021-08-31 10:08:29 +0700

    Implement log middleware

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"users-api/config"
 	"users-api/routes"
+	m "users-api/middlewares"
 )
 
 func main() {
 	config.InitDB()
 	e := routes.New()
+
+	// logger middleware
+	m.LogMiddlewares(e)
 	e.Logger.Fatal(e.Start(":8000"))
 }
